fd751abad81d7c2ffbb7fa9ff51a034d4b11d450
Vincenzo Menna <68065413+vimenn@users.noreply.github.com>
2026-02-11 13:47:27 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+﻿const revealElements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+revealElements.forEach(el => observer.observe(el));
